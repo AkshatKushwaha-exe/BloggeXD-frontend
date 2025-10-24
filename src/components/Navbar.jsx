b9f8372692b7beb8d3f77c578824e5fafa88a7dc
AKSHAT KUSHWAHA <akshat.24bcs10060@sst.scaler.com>
2025-10-24 21:26:31 +0530

    Name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import "../styles/Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-brand">My MERN Blog</Link>
+      <Link to="/" className="nav-brand">BloggeXD</Link>
       <div className="nav-links">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/create" className="nav-link">Create Post</Link>
